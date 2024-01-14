926e81baf804556a97a5f63682df2ae420109a79
Ruslan Dev <email.com>
2024-01-14 12:54:00 +0300

    final

---
+++ README.md
@@ -0,0 +1,3 @@
+Challenge: https://www.frontendmentor.io/challenges/github-user-search-app-Q09YOgaH6
+
+Dev Finder App: React + TS
---
+++ src/mock/index.ts
@@ -0,0 +1,17 @@
+import { LocalGithubUser } from 'types'
+
+export const defaultUser: LocalGithubUser = {
+	login: 'rruslandev',
+	avatar:
+		'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJ1-NjADp9p86e4M1s7zJjHs7sdNYqRgXNbg&usqp=CAU',
+	name: 'Ruslan Rasulov',
+	company: 'Yandex',
+	blog: 'yandex.ru',
+	location: 'Moscow',
+	bio: 'Fullstack developer',
+	twitter: 'tiger',
+	repos: 80,
+	followers: 777,
+	following: 1,
+	created: '2021-07-01T08:18:45Z',
+}
